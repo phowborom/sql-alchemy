c3396e892297f65f16d9e74e5b8326224b5b2098
Tom Phowborom <78992395+phowborom@users.noreply.github.com>
2021-06-24 21:17:13 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 import datetime as dt
+import numpy as np
 
 from flask import Flask, jsonify
 
